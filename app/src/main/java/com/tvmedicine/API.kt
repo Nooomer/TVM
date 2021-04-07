4d4c7c94d0dd0a46607b41273b22b12b2d53e7e4
Kostya Ampilogov <ampilogov.qaz@gmail.com>
2021-04-07 17:23:35 +0500

    Alert dialog implementation.

--- app/src/main/java/com/tvmedicine/API.kt
+++ app/src/main/java/com/tvmedicine/API.kt
@@ -4,7 +4,7 @@ class API {
     fun getUsers():String{
         return "https://1515714571567515.000webhostapp.com/getUsers.php"
     }
-    fun doctorAuth(number: Int, password:Int):String{
+    fun doctorAuth(number: String, password:String):String{
         return "https://1515714571567515.000webhostapp.com/doctorAuth.php?number=$number&password=$password"
     }
 }
--- app/src/main/java/com/tvmedicine/MainActivity.kt
+++ app/src/main/java/com/tvmedicine/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
@@ -17,14 +18,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
     fun doctor_auth_btn(view: View){
-        val input: EditText = EditText(this)
-        AlertDialog.Builder(this)
-                .setTitle("Update Status")
-                .setMessage("message")
-                .setView(input)
-                .setPositiveButton("Ok") { dialog, whichButton -> val value: Editable = input.text }.setNegativeButton("Cancel") { dialog, whichButton ->
-                    // Do nothing.
-                }.show()
+        val li: LayoutInflater = LayoutInflater.from(this);
+        val alertView: View = li.inflate(R.layout.alert, null);
+
+        //Создаем AlertDialog
+       val mDialogBuilder: AlertDialog.Builder  = AlertDialog.Builder(this);
+
+        //Настраиваем alert.xml для нашего AlertDialog:
+        mDialogBuilder.setView(alertView);
+
+        //Настраиваем отображение поля для ввода текста в открытом диалоге:
+        val userInput1: EditText  = alertView.findViewById(R.id.phone_number_field);
+        val userInput2: EditText  = alertView.findViewById(R.id.password_field);
+        val api: API = API()
+        mDialogBuilder
+                .setCancelable(true)
+                .setPositiveButton(getString(R.string.login_btn)) { _: DialogInterface, _: Int ->
+                    api.doctorAuth(userInput1.text.toString(),userInput2.text.toString())
+                }
+                .setPositiveButton(getString(R.string.cancel_btn)) { dialogInterface: DialogInterface, _: Int ->
+                    dialogInterface.cancel()
+                }
+        val alertDialog: AlertDialog = mDialogBuilder.create();
+        alertDialog.show();
+
     }
     fun click(view: View){
         val intent = Intent(this@MainActivity, debug::class.java)
