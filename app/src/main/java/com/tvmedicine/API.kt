1a7211afaec1756e255b639e82a83ad806092839
Kostya Ampilogov <ampilogov.qaz@gmail.com>
2021-04-27 12:17:01 +0500

    Add coroutines construction

--- app/src/main/java/com/tvmedicine/API.kt
+++ app/src/main/java/com/tvmedicine/API.kt
@@ -1,8 +1,15 @@
 package com.tvmedicine
 
 
-import androidx.lifecycle.LiveData
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleCoroutineScope
+import androidx.lifecycle.LifecycleOwner
 import com.beust.klaxon.Klaxon
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
@@ -14,14 +21,12 @@ class API {
     fun getUsers():String{
         return "https://1515714571567515.000webhostapp.com/getUsers.php"
     }
-    fun doctorAuth(number: String, password:String): Boolean{
-        val lgc: Boolean
-        val url = "https://1515714571567515.000webhostapp.com/doctorAuth.php?phone_number=$number&password=$password"
-                    val doc: Document = Jsoup
-                        .connect(url)
-                        .get()
-                    val BodyText = doc.select("body")
-                   lgc = BodyText.text().toBoolean()
-        return lgc
+     fun doctorAuth(number: String, password:String): Boolean {
+         var lgc: Boolean = true
+         val ntwrk = NetworkThread()
+         CoroutineScope(Dispatchers.IO).launch {
+             lgc = ntwrk.frst(number, password)
+         }
+         return lgc
+     }
     }
-}
--- app/src/main/java/com/tvmedicine/NetworkThread.kt
+++ app/src/main/java/com/tvmedicine/NetworkThread.kt
@@ -1,4 +1,20 @@
 package com.tvmedicine
 
-class NetworkThread {
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+
+class NetworkThread() {
+     suspend fun frst(number: String, password:String):Boolean = withContext(Dispatchers.IO){
+        val lgc: Boolean
+        val url = "https://1515714571567515.000webhostapp.com/doctorAuth.php?phone_number=$number&password=$password"
+        val doc: Document = Jsoup
+            .connect(url)
+            .get()
+        val BodyText = doc.select("body")
+        lgc = BodyText.text().toBoolean()
+        return@withContext true
+    }
 }
