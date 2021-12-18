0d8cf154922065d821379799491ffd0a996b7e33
Nooomer <ampilogov.qaz@gmail.com>
2021-12-19 02:06:47 +0500

    Fix TreatmentModel.kt

--- app/src/main/java/com/tvmedicine/models/TreatmentModel.kt
+++ app/src/main/java/com/tvmedicine/models/TreatmentModel.kt
@@ -2,6 +2,7 @@ package com.tvmedicine.models
 
 data class TreatmentModel(
         val id : Int,
+        val chatId: Int,
         val patientId : Int,
         val doctorId : Int,
         val startDate: String,
