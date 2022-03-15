58a6358cd3183886ec780db4e82becfc6356c3ad
Nooomer <ampilogov.qaz@gmail.com>
2022-03-15 08:43:00 +0500

    Add file download method.

--- app/src/main/java/com/tvmedicine/UploadRecord.kt
+++ app/src/main/java/com/tvmedicine/UploadRecord.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.*
 import org.apache.commons.net.ftp.FTPClient
 import java.io.FileInputStream
 import java.io.FileNotFoundException
+import java.io.FileOutputStream
 import java.io.IOException
 
 class UploadRecord {
@@ -34,5 +35,24 @@ class UploadRecord {
             def.await()
             }
         }
+    @Throws(FileNotFoundException::class)
+    fun download() {
+        val scope = CoroutineScope(Dispatchers.Main + Job())
+        val fClient = FTPClient()
+        val fInput = FileOutputStream(path)
+        val fs = "/www/u1554079.isp.regruhosting.ru/audio/$name.wav"
+        scope.launch {
+            val def = scope.asyncIO { val hostAddress = "31.31.196.105"
+                fClient.connect(hostAddress)
+                fClient.enterLocalPassiveMode()
+                val log = "u1554079"
+                val password = "y8P22m5yJwPGS8Yw"
+                fClient.login(log, password)
+                fClient.retrieveFile(fs, fInput)
+                fClient.logout()
+                fClient.disconnect() }
+            def.await()
+        }
+    }
 
     }
