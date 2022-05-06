e6f808686065474918c0af92b2754dcdc31b1bf6
Nooomer <ampilogov.qaz@gmail.com>
2022-05-06 21:11:10 +0500

    Add if state for check playing

--- app/src/main/java/com/tvmedicine/ChatActivity.kt
+++ app/src/main/java/com/tvmedicine/ChatActivity.kt
@@ -4,18 +4,14 @@ import android.content.SharedPreferences
 import android.graphics.Color
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.os.PersistableBundle
 import android.provider.MediaStore.Audio
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
 import android.view.animation.OvershootInterpolator
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -70,18 +66,23 @@ class ChatActivity : AppCompatActivity() {
         var itemCount = recyclerView.adapter?.itemCount
         itemCount = 0
         load(scope, result, recyclerView,itemCount)
-
         audioButton = findViewById<View>(R.id.start_button).apply {
             setOnClickListener { onSendMessageButtonClicked() }
         }
+        var mLastClickTime: Long = 0
+        val TIME_INTERVAL: Long = 1000
+        var playing:Boolean = false
         playButton = findViewById<View>(R.id.play_button).apply {
             setOnClickListener {
-
-
-
-                player.play("10","1")
-
-
+                if (!playing) {
+                    player.play("10","1")
+                    mLastClickTime = System.currentTimeMillis();
+                    playing = true;
+                } else {
+                    player.stop()
+                    playing = false
+                }
+                //player.play("10","1")
             }
         }
         sendMessageButton.setOnClickListener {
@@ -116,6 +117,7 @@ class ChatActivity : AppCompatActivity() {
             }
         }
     }
+
     private fun onSendMessageButtonClicked() {
         if (recordController.isAudioRecording()) {
             recordController.stop()
