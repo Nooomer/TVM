ba50e022e6ba8f04cef1f0c1ec80255d695d54ce
Nooomer <ampilogov.qaz@gmail.com>
2022-05-18 20:42:28 +0500

    Change to ChatActivity

--- app/src/main/java/com/tvmedicine/WindowFragment.kt
+++ app/src/main/java/com/tvmedicine/WindowFragment.kt
@@ -19,37 +19,37 @@ class WindowFragment : BottomSheetDialogFragment(), OnBottomSheetCallbacks {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        (activity as TreatmentActivity).setOnBottomSheetCallbacks(this)
+        (activity as ChatActivity).setOnBottomSheetCallbacks(this)
 
         return inflater.inflate(R.layout.fragment_window, container, false)
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val textResult = requireView().findViewById<AppCompatTextView>(R.id.textResult)
-        val filterImage = requireView().findViewById<ImageButton>(R.id.filterImage)
-        textResult.setOnClickListener {
-            (activity as TreatmentActivity).openBottomSheet()
-        }
-        filterImage.setOnClickListener {
-            if (currentState == BottomSheetBehavior.STATE_EXPANDED) {
-                (activity as TreatmentActivity).closeBottomSheet()
-            } else  {
-                (activity as TreatmentActivity).openBottomSheet()
-            }
-        }
+        //val textResult = requireView().findViewById<AppCompatTextView>(R.id.textResult)
+        //val filterImage = requireView().findViewById<ImageButton>(R.id.filterImage)
+        //textResult.setOnClickListener {
+            //(activity as TreatmentActivity).openBottomSheet()
+       // }
+        //filterImage.setOnClickListener {
+            //if (currentState == BottomSheetBehavior.STATE_EXPANDED) {
+               // (activity as TreatmentActivity).closeBottomSheet()
+            //} else  {
+                //(activity as TreatmentActivity).openBottomSheet()
+            //}
+        //}
     }
     override fun onStateChanged(bottomSheet: View, newState: Int) {
-        val textResult = requireView().findViewById<AppCompatTextView>(R.id.textResult)
-        val filterImage = requireView().findViewById<ImageButton>(R.id.filterImage)
+        //val textResult = requireView().findViewById<AppCompatTextView>(R.id.textResult)
+        //val filterImage = requireView().findViewById<ImageButton>(R.id.filterImage)
         currentState = newState
         when (newState) {
             BottomSheetBehavior.STATE_EXPANDED -> {
-                textResult.text = "0 results"
-                filterImage.setImageResource(R.drawable.ic_baseline_filter_list_24)
+                //textResult.text = "0 results"
+                //filterImage.setImageResource(R.drawable.ic_baseline_filter_list_24)
             }
             BottomSheetBehavior.STATE_COLLAPSED -> {
-                textResult.text = "See the results"
-                filterImage.setImageResource(R.drawable.ic_baseline_keyboard_arrow_up_24)
+                //textResult.text = "See the results"
+                //filterImage.setImageResource(R.drawable.ic_baseline_keyboard_arrow_up_24)
             }
         }
     }
