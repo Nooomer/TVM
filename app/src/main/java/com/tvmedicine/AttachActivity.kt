96993e04e0366328d31d86079ee9008b65b5d032
Nooomer <ampilogov.qaz@gmail.com>
2022-05-22 21:46:13 +0500

    Listener for play audio

--- app/src/main/java/com/tvmedicine/AttachActivity.kt
+++ app/src/main/java/com/tvmedicine/AttachActivity.kt
@@ -1,7 +1,10 @@
 package com.tvmedicine
 
+import RecyclerItemClickListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.tvmedicine.RetrifitService.Common
@@ -9,8 +12,13 @@ import com.tvmedicine.models.MessagesModel
 import kotlinx.coroutines.*
 
 class AttachActivity : AppCompatActivity() {
+    private var position: Int = 0
+    private var player: AppVoicePlayer = AppVoicePlayer(this)
     lateinit var recyclerView: RecyclerView
     var result: List<MessagesModel?>? = null
+    var lastPlayPosition = 0
+    var audio_message_id = mutableListOf<Int>()
+    var audio_message_datetime = mutableListOf<String?>()
     var data_string = Array<String>(10){""}
     private var viewSize: Int = 0
     val scope = CoroutineScope(Dispatchers.Main + Job())
@@ -22,6 +30,54 @@ class AttachActivity : AppCompatActivity() {
         recyclerView = findViewById(R.id.rv_view3)
         recyclerView.layoutManager = LinearLayoutManager(this)
         load(scope, result, recyclerView,0)
+        val recyclerView = findViewById<RecyclerView>(R.id.rv_view3)
+        var playing = false
+        recyclerView.addOnItemTouchListener(
+            RecyclerItemClickListener(
+                this,
+                recyclerView,
+                object : RecyclerItemClickListener.OnItemClickListener {
+                    override fun onItemClick(view: View?, position: Int) {
+                        this@AttachActivity.position = position
+                        playing = if (!playing) {
+                            player.play(audio_message_id[position].toString(),"1")
+                            lastPlayPosition = position
+                            true
+                        } else {
+                            if ((lastPlayPosition != position) and (playing)) {
+                                player.stop()
+                                player.play(audio_message_id[position].toString(), "1")
+                                lastPlayPosition = position
+                                true
+                            } else {
+                                if ((lastPlayPosition == position)and(!playing)) {
+                                    player.play(audio_message_id[position].toString(), "1")
+                                    lastPlayPosition = position
+                                    true
+                                }
+                                else{
+                                    if ((lastPlayPosition == position)and(playing)) {
+                                        player.play(audio_message_id[position].toString(), "1")
+                                        lastPlayPosition = position
+                                        true
+                                    }
+                                    else{
+                                        player.stop()
+                                        false
+                                    }
+
+                                }
+                                }
+                        }
+                        }
+//
+
+
+                    override fun onLongItemClick(view: View?, position: Int) {
+                        Toast.makeText(this@AttachActivity,"Голосовое сообщение", Toast.LENGTH_LONG).show()
+                    }
+                })
+        )
     }
     private fun allMessageRequest(): List<MessagesModel?>? {
         val mService = Common.retrofitService
@@ -31,6 +87,7 @@ class AttachActivity : AppCompatActivity() {
         result = call?.execute()?.body()
         return result
     }
+
     private fun load(scope: CoroutineScope, result: List<MessagesModel?>?, recyclerView: RecyclerView, itemCount: Int?){
         var result1 = result
         scope.launch {
@@ -44,11 +101,12 @@ class AttachActivity : AppCompatActivity() {
                    // messageDate = result1?.get(i)?.message_datetime.toString()
                     if(result1!![i]?.sound_server_link!=null)
                     {
-                        data[i][1] = result1?.get(i)?.message_datetime
-                        data_string[i] = result1!![i]?.message_id.toString()
+                        audio_message_datetime.add(result1?.get(i)?.message_datetime)
+                        //data_string[i] = result1!![i]?.message_id.toString()
+                        audio_message_id.add(i+1)
                     }
                 }
-                recyclerView.adapter = soundRvAdapter(data)
+                recyclerView.adapter = soundRvAdapter(audio_message_datetime, audio_message_id.size)
             }
         }
     }
